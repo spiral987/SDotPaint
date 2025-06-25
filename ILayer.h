a55ef6614af51fac099499cb14cbeedfe253e501
spiral987 <41581839+spiral987@users.noreply.github.com>
2025-06-25 19:29:40 +0900

    なめらかな線

---
+++ ILayer.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <windows.h>
+#include "PenData.h" // PenPoint構造体のため
+#include <vector>
+
+// すべてのレイヤーの基底となるインターフェースクラス
+class ILayer
+{
+public:
+    // 仮想デストラクタは必須です
+    virtual ~ILayer() = default;
+
+    // 純粋仮想関数（このクラスを継承するクラスは必ず実装しなければならない）
+    virtual void draw(HDC hdc) const = 0;         // 描画関数
+    virtual void addPoint(const PenPoint &p) = 0; // 点を追加する関数
+    virtual void clear() = 0;                     // レイヤーをクリアする関数
+    virtual void startNewStroke() = 0;            // 新しい線が始まる命令
+
+    virtual const std::vector<std::vector<PenPoint>> &getStrokes() const = 0; // 点のリストを取得する関数(テスト用)
+};
---
+++ VectorLayer.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "ILayer.h"
+#include <vector>
+
+class VectorLayer : public ILayer
+{
+private:
+    std::vector<std::vector<PenPoint>> strokes_;
+
+public:
+    void draw(HDC hdc) const override;
+    void addPoint(const PenPoint &p) override;
+    void clear() override;
+
+    void startNewStroke() override;
+    const std::vector<std::vector<PenPoint>> &getStrokes() const override;
+};
